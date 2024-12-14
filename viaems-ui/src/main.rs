c377b0ab9e7462f989a4dae3875cfc2368ff8e65
Matthew Via <via@matthewvia.info>
2024-12-14 13:37:36 -0500

    ui updates

--- viaems-ui/src/main.rs
+++ viaems-ui/src/main.rs
@@ -1,78 +1,118 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
+use rfd;
 use viaems::{self, interface, connection};
-use std::sync::{Arc, Mutex, atomic};
+use std::sync::{Arc, Mutex};
 use std::time::SystemTime;
 
 #[derive(Default)]
 struct FeedState {
     keys: Vec<String>,
     values: Vec<interface::FeedValue>,
+    update_time: Option<SystemTime>,
+}
+
+struct Application {
+    target: Option<viaems::Manager>,
+    log: Option<viaems::LogReader>,
+    latest_feed: Arc<Mutex<FeedState>>,
+}
+
+impl Application {
+    fn new() -> Application { 
+        let feed = Arc::new(Mutex::new(FeedState::default()));
+        Application{ target: None, log: None , latest_feed: feed} 
+    }
+
+    fn open_log(&mut self, filename: &str) {
+        self.log = Some(viaems::LogReader::new(filename));
+    }
+
+    fn connect_udp(&mut self) {
+        let conn = Box::new(connection::UdpConnection::new("127.0.0.1:5556", "127.0.0.1:5555"));
+        //    let conn = Box::new(connection::UsbConnection::new());
+        let target = viaems::Manager::new(conn);
+        target.on_feed({
+            let feed_state = self.latest_feed.clone();
+            move |time: SystemTime, keys: &Vec<String>, values: &Vec<interface::FeedValue>| {
+                Application::update_feed(&feed_state, time, keys, values)
+            }});
+                
+        self.target = Some(target);
+    }
+
+    fn update_feed(feed_state: &Arc<Mutex<FeedState>>, _: SystemTime, keys: &Vec<String>, values: &Vec<interface::FeedValue>) {
+        let mut state = feed_state.lock().unwrap();
+        if state.keys.len() != keys.len() {
+            state.keys = keys.clone();
+        }
+        state.values = values.clone();
+        state.update_time = Some(SystemTime::now());
+    }
+
+
 }
 
 fn main() -> Result<(), eframe::Error> {
 
-//    let conn = Box::new(connection::UdpConnection::new("127.0.0.1:5556", "127.0.0.1:5555"));
-    let conn = Box::new(connection::UsbConnection::new());
-    let manager = viaems::Manager::new(conn);
-
     let options = eframe::NativeOptions::default();
 
-    let count = Arc::new(atomic::AtomicUsize::new(0));
 
-    let feed_state = Arc::new(Mutex::new(FeedState::default()));
+    let mut state = Application::new();
 
-    manager.on_feed({
-        let count = count.clone();
-        let feed_state = feed_state.clone();
-        move |_: SystemTime, keys: &Vec<String>, values: &Vec<interface::FeedValue>| {
-            let mut state = feed_state.lock().unwrap();
-            if state.keys.len() != keys.len() {
-                state.keys = keys.clone();
-            }
-            state.values = values.clone();
-            count.fetch_add(1, atomic::Ordering::Relaxed);
-        }});
+    eframe::run_simple_native("Viaems UI", options, move |ctx, _frame| {
+        ctx.set_visuals(egui::Visuals::light());
+        ctx.set_pixels_per_point(1.5);
 
-    // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
 
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
-        egui::SidePanel::left("left panel").show(ctx, |ui| {
-          ui.label("Live Data");
-          let state = feed_state.lock().unwrap();
-          egui::ScrollArea::vertical().show(ui, |ui| {
-              egui::Grid::new("feed")
-                  .num_columns(2)
-                  .striped(true)
-                  .show(ui, |ui| {
-                      for (k, v) in state.keys.iter().zip(state.values.iter()) {
-                          ui.label(k);
-                          ui.label(v.to_string());
-                          ui.end_row();
-                      }
+        egui::TopBottomPanel::top("Menubar").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("Log", |ui| {
+                    if ui.button("Open log").clicked() {
+                        if let Some(path) = rfd::FileDialog::new().pick_file() {
+                            let picked_path = path.display().to_string();
+                            state.open_log(&picked_path);
+                        }
+                    }
+                });
+                ui.menu_button("_Target", |ui| {
+                    if ui.button("Open UDP").clicked() {
+                            state.connect_udp();
+                    }
                 });
             });
         });
-        egui::TopBottomPanel::bottom("bottom").show(ctx, |ui| {
-            ui.label("Bottom");
+        if state.target.is_some() {
+            egui::SidePanel::left("left panel").show(ctx, |ui| {
+              ui.label("Live Data");
+              let state = state.latest_feed.lock().unwrap();
+              egui::ScrollArea::vertical().show(ui, |ui| {
+                  egui::Grid::new("feed")
+                      .num_columns(2)
+                      .striped(true)
+                      .show(ui, |ui| {
+                          for (k, v) in state.keys.iter().zip(state.values.iter()) {
+                              ui.label(k);
+                              ui.label(v.to_string());
+                              ui.end_row();
+                          }
+                    });
+                });
+            });
+        }
+        egui::TopBottomPanel::bottom("Status").show(ctx, |ui| {
+            match &state.target {
+                None => ui.label("Target: Not connected"),
+                Some(_) => ui.label("Target: Connected!"),
+            };
+            match &state.log {
+                None => ui.label("Log: Not connected"),
+                Some(log) => ui.label("Log: ".to_owned() + log.filename()),
+            };
         });
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("test");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                age += 1;
-            }
-            ui.label(format!("Hello '{name}', age {age}"));
-            let current_count = count.load(atomic::Ordering::Relaxed);
-            ui.label(format!("Current count: {current_count}"));
+            ui.heading("TODO");
         });
         ctx.request_repaint();
     })
