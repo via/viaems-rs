188669a09bdb02b7088ba85904bc55184b2b79f8
Matthew Via <via@matthewvia.info>
2026-02-01 17:24:25 -0500

    light connection refactoring

--- viaems/src/connection/exec.rs
+++ viaems/src/connection/exec.rs
@@ -1,5 +1,5 @@
 use std::sync::{mpsc, atomic, Arc};
-use crate::connection::{Connection, RxMessage};
+use crate::connection::{stream, Connection, RxMessage};
 use crate::interface;
 use std::io::{BufRead, BufReader, Write};
 
@@ -19,48 +19,34 @@ impl Connection {
 
         let rx_thr = std::thread::spawn({
             let stdout = subproc.stdout.take().unwrap();
-            let mut buffered_stdout = BufReader::new(stdout);
+            let mut stream_reader = stream::StreamReader::new(stdout);
             let running = running.clone();
             move || {
-                let mut raw_bytes = vec![];
-                let CRC32 = crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
                 loop {
                     if !running.load(atomic::Ordering::Relaxed) {
                         break;
                     }
 
-                    raw_bytes.clear();
-                    buffered_stdout.read_until(0, &mut raw_bytes).expect("failed to read bytes");
-
-                    // Len + CRC is 6 byte minimum
-                    if raw_bytes.len() < 6 || *raw_bytes.last().unwrap() != 0 {
-                        break;
-                    }
-
-                    let decoded_size = cobs::decode_in_place(raw_bytes.as_mut_slice()).expect("failed to decode cobs frame");
-                    raw_bytes.truncate(decoded_size);
-
-                    let pdu = &raw_bytes[2..raw_bytes.len()-4];
-
-                    let len = u16::from_le_bytes(raw_bytes[0..2].try_into().unwrap());
-                    if len as usize != pdu.len() {
-                        println!("Frame is invalid length!");
-                        continue;
-                    }
-                    let crc = u32::from_le_bytes(raw_bytes[decoded_size-4..decoded_size].try_into().unwrap());
-                    if crc != CRC32.checksum(pdu) {
-                        println!("Invalid CRC");
-                        continue;
-                    }
-                    match prost::Message::decode(pdu) {
-                        Ok(message) => {
-                            let time = SystemTime::now();
-                            if recv_tx.send(RxMessage{time, message}).is_err() { break; }
+                    match stream_reader.read() {
+                        Err(stream::Error::IOError(x)) => {
+                            println!("Failed to read from target: {}", x);
+                            break;
                         },
-                        Err(e) => {
-                            println!("Failed to decode! {e}");
-                        },
-                    }
+                        Err(stream::Error::FrameDecodeError) => continue,
+                        Ok(pdu) => {
+                            match prost::Message::decode(pdu.as_slice()) {
+                                Ok(message) => {
+                                    let time = SystemTime::now();
+                                    if recv_tx.send(RxMessage{time, message}).is_err() { break; }
+                                },
+                                Err(e) => {
+                                    println!("Failed to decode! {e}");
+                                    continue;
+                                },
+                            };
+                        }
+                    };
+
                 }
             }});
 
@@ -71,7 +57,6 @@ impl Connection {
             let mut stdin = subproc.stdin.take().unwrap();
             let running = running.clone();
             move || {
-                let CRC32 = crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
                 loop {
                     if !running.load(atomic::Ordering::Relaxed) {
                         break;
@@ -79,18 +64,7 @@ impl Connection {
                     match send_rx.recv_timeout(Duration::from_millis(100)) {
                         Ok(command) => {
                             let pdu = prost::Message::encode_to_vec(&command);
-                            let len_bytes = (pdu.len() as u16).to_le_bytes();
-                            let crc_bytes = CRC32.checksum(pdu.as_slice()).to_le_bytes();
-
-                            let mut frame = Vec::with_capacity(len_bytes.len() + pdu.len() + crc_bytes.len());
-                            frame.extend_from_slice(&len_bytes);
-                            frame.extend_from_slice(pdu.as_slice());
-                            frame.extend_from_slice(&crc_bytes);
-
-                            let mut cobs = cobs::encode_vec(frame.as_slice());
-                            cobs.extend_from_slice(&[0 as u8]);
-
-                            stdin.write(cobs.as_slice()).unwrap();
+                            stdin.write(stream::write(pdu.as_slice()).as_slice()).unwrap();
                         }
                         Err(mpsc::RecvTimeoutError::Timeout) => continue,
                         _ => break,
--- viaems/src/connection/mod.rs
+++ viaems/src/connection/mod.rs
@@ -65,3 +65,75 @@ impl Connection {
         Writer { tx: self.tx.clone() }
     }
 }
+
+
+mod stream {
+    use std::io::{BufRead, BufReader, Read, Write};
+    pub enum Error {
+        IOError(std::io::Error),
+        FrameDecodeError,
+    }
+
+    impl From<std::io::Error> for Error {
+        fn from(inner: std::io::Error) -> Error {
+            Error::IOError(inner)
+        }
+    }
+
+
+    pub struct StreamReader<T> {
+        inner: BufReader<T>,
+        crc: crc::Crc<u32>,
+    }
+
+    impl<T: Read> StreamReader<T> {
+        pub fn new(input: T) -> Self {
+            Self {
+                inner: BufReader::new(input),
+                crc: crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC),
+            }
+        }
+
+        pub fn read(&mut self) -> Result<Vec<u8>, Error> {
+            let mut raw_bytes = vec![];
+            self.inner.read_until(0, &mut raw_bytes)?;
+
+            if raw_bytes.len() < 6 || *raw_bytes.last().unwrap() != 0 {
+                return Err(Error::FrameDecodeError);
+            }
+
+            let decoded_size = cobs::decode_in_place(raw_bytes.as_mut_slice()).expect("failed to decode cobs frame");
+            raw_bytes.truncate(decoded_size);
+
+            let pdu = &raw_bytes[2..raw_bytes.len()-4];
+
+            let len = u16::from_le_bytes(raw_bytes[0..2].try_into().unwrap());
+            if len as usize != pdu.len() {
+                return Err(Error::FrameDecodeError);
+            }
+
+            let crc = u32::from_le_bytes(raw_bytes[decoded_size-4..decoded_size].try_into().unwrap());
+            if crc != self.crc.checksum(pdu) {
+                return Err(Error::FrameDecodeError);
+            }
+
+            Ok(pdu.to_owned())
+        }
+    }
+
+    pub fn write(pdu: &[u8]) -> Vec<u8> {
+        let crc = crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
+        let len_bytes = (pdu.len() as u16).to_le_bytes();
+        let crc_bytes = crc.checksum(pdu).to_le_bytes();
+
+        let mut frame = Vec::with_capacity(len_bytes.len() + pdu.len() + crc_bytes.len());
+        frame.extend_from_slice(&len_bytes);
+        frame.extend_from_slice(pdu);
+        frame.extend_from_slice(&crc_bytes);
+
+        let mut cobs = cobs::encode_vec(frame.as_slice());
+        cobs.extend_from_slice(&[0 as u8]);
+
+        cobs
+    }
+}
